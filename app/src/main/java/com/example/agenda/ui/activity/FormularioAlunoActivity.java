f7f3b5f8ecdc3db47dd761a5c4d08fc0395dfbbb
Eduardo Frésca <eduardomfresca@gmail.com>
2021-12-22 11:56:01 -0300

    Criação da class aluno e alunoDAO, e método salvar

--- app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,9 +2,16 @@ package com.example.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.agenda.DAO.AlunoDAO;
 import com.example.agenda.R;
+import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +19,24 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        AlunoDAO dao = new AlunoDAO();
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(view -> {
+            String nome = campoNome.getText().toString();
+            String telefone = campoTelefone.getText().toString();
+            String email = campoEmail.getText().toString();
+
+            Aluno alunoCriado = new Aluno(nome, telefone, email);
+            dao.salva(alunoCriado);
+
+            startActivity(new Intent(FormularioAlunoActivity.this,
+                    ListaAlunosActivity.class));
+        });
     }
 }
