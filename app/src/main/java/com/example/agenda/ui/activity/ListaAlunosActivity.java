5fc6af4922f5dba29a4e279546caaf848fcf78a2
Eduardo Frésca <eduardomfresca@gmail.com>
2021-12-30 09:49:30 -0300

    Mudança dataset

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -5,13 +5,9 @@ import static com.example.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -23,9 +19,6 @@ import com.example.agenda.R;
 import com.example.agenda.model.Aluno;
 import com.example.agenda.ui.adapter.ListaAlunosAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -81,8 +74,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void atualizaAlunos() {
-        adapter.clear();
-        adapter.addAll(dao.todos());
+        adapter.atualiza(dao.todos());
     }
 
     private void configuraLista() {
